1385fc7b9ee0c490e520e5feaacabe14dfb94e7b
rakati <ouhaddounouredine@gmail.com>
2024-09-26 17:42:10 +0100

    add public_url basename

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
+import LandingPage from './components/LandingPage';
 import Register from './components/Register';
 import Profile from './components/Profile';
 import Settings from './components/Settings';
@@ -7,9 +9,10 @@ import Chat from './components/Chat';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL} >
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/settings" element={<Settings />} />
